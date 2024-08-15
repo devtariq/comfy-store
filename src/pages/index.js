4051f0ed273eb0bde4b44134cf6d209b52a585f7
devtariq <metariqul91@gmail.com>
2024-08-15 10:32:43 +0600

    comfy-react start and error page done

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage';
+import Products from './ProductsPage.js'
+import Cart from './CartPage.js';
+import SingleProduct from './SingleProductPage.js';
+import Error from './ErrorPage.js';
+import About from './AboutPage.js';
+import Checkout from './CheckoutPage.js';
+import PrivateRoute from './PrivateRoute.js'
+
+export {Home, Products,Cart, SingleProduct, Error,About, Checkout,PrivateRoute};
